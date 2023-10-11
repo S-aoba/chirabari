bc2653d99b20a14aea7148f69acbf7f92a5584f7
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-10-11 21:55:00 +0900

    🔧 add prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "always",
+  htmlWhitespaceSensitivity: 'strict',
+  trailingComma: 'es5',
+};
