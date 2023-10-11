93c6211423a53c706449ace62af049d031221c31
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-10-11 22:00:15 +0900

    🩹 format prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Next13 starter template',
   description: 'Next13 starter template',
-};
+}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ja' suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
