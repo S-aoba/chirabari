7e4eae7de996b3cd2639661742bc2fffabb9ba02
S-aoba <writer_fairy_road@yahoo.co.jp>
2023-10-12 17:26:15 +0900

    add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,63 @@
-module.exports ={
-  extends: ["next/core-web-vitals", "prettier"],
-  parser: "@typescript-eslint/parser",
+module.exports = {
+  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { project: './tsconfig.json', tsconfigRootDir: __dirname, sourceType: 'module' },
+  plugins: ['import-access', 'simple-import-sort', 'react-hooks'],
+  rules: {
+    'no-console': ['error', { allow: ['warn', 'info', 'error'] }],
+    'prefer-arrow-callback': 'error',
+    'prefer-const': 'error',
+    'func-style': ['error', 'expression'],
+    'arrow-body-style': ['error', 'always'],
+    'react/prop-types': 'off',
+    'react/jsx-handler-names': [
+      'error',
+      {
+        eventHandlerPrefix: 'handle',
+        eventHandlerPropPrefix: 'on',
+        checkLocalVariables: true,
+        checkInlineFunction: false,
+      },
+    ],
+    'react/destructuring-assignment': ['error', 'always'],
+
+    // import
+    'no-restricted-imports': ['error', { paths: [{ name: 'react', importNames: ['default'] }] }],
+    'import/no-default-export': 'error',
+    'import-access/jsdoc': ['error'],
+    'import/newline-after-import': 'error',
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+
+    // react-hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+
+    // typescript
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-var-requires': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/consistent-type-imports': ['warn', { prefer: 'type-imports' }],
+    '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^_', argsIgnorePattern: '^_' }],
+    '@typescript-eslint/naming-convention': [
+      'error',
+      { selector: ['typeAlias', 'typeParameter'], format: ['PascalCase'] },
+      { selector: ['classProperty', 'method'], format: ['camelCase'] },
+      {
+        selector: 'variable',
+        types: ['boolean'],
+        format: ['PascalCase'],
+        prefix: ['no', 'is', 'has', 'should'],
+        filter: { regex: '^_', match: false },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['src/app/**/*.{ts,tsx}'],
+      rules: {
+        'import/no-default-export': 'off',
+      },
+    },
+  ],
 }
